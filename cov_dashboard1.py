07d1a99ed94b7537b9d3d16151d7fb1b66f1325d
Azadi97 <gworld97@gmail.com>
2022-04-08 09:53:45 +0200

    Update and rename cov_dashboard.py to cov_dashboard1.py

--- cov_dashboard1.py
+++ cov_dashboard1.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 # Read the data
 # deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
-df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-
+# df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
 
 # Get the countries list
 clist = df['Country/Region'].unique()
@@ -16,17 +16,14 @@ clist = df['Country/Region'].unique()
 country = st.sidebar.selectbox("Select a country:",clist)
 
 # The header of the figure
-st.header("GDP per Capita over time")
+st.header("COVID-19 cases per country")
 
 
 # Create a figure
-df_usa_new_cases = df[['Country/Region', '3/31/22']].reset_index()
+fig = plt.figure()
+plt.plot(df[df["location"]==country].iloc[[0]].iloc[:].values[0])
 
-# use the plot function
-fig = plt.figure(figsize=(15,5))
-plt.plot(df_usa_new_cases['3/31/22'])
-plt.legend(['Describtion for line'])
-plt.title('Title goes here')
+plt.title("Cumulative cases in " + country)
 
 
 # Plots the chart
