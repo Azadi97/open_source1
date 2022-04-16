8e623aceeaa5c9127338b72a3081f08318fc9c85
Azadi97 <gworld97@gmail.com>
2022-04-16 13:35:34 +0200

    Update cov_dashboard1.py

--- cov_dashboard1.py
+++ cov_dashboard1.py
@@ -1,43 +1,57 @@
-# import streamlit as st
-# import pandas as pd
-# import plotly.express as px
-# import matplotlib.pyplot as plt
-
-# # Read the data
-# # deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
-
-# # df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-# df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
-
-# # Get the countries list
-# clist = df['Country/Region'].unique()
-
-# # Create the streamlit sidebar
-# country = st.sidebar.selectbox("Select a country:",clist)
-
-# # The header of the figure
-# st.header("COVID-19 cases per country")
-
-
-# # Create a figure
-# fig = plt.figure()
-# plt.plot(df[df["location"]==country].iloc[[0]].iloc[:].values[0])
-
-# plt.title("Cumulative cases in " + country)
-
-
-# # Plots the chart
-# st.plotly_chart(fig)
-
-
 import streamlit as st
 import pandas as pd
-# import plotly.express as px
-# import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+@st.cache(suppress_st_warning=True) 
+def loadData():
+    st.write("Cache miss: Loading data")
+    df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+    df["date"] = pd.to_datetime(df["date"])
+    return df
 
-cases = df[df['location'] == 'Afghanistan']['new_cases']
-dates = df[df['location'] == 'Afghanistan']['date']
+@st.cache(suppress_st_warning=True) 
+def getCountriesList(df):
+    st.write("Cache miss: Getting countries list")
+    # Get the countries list
+    clist = df['location'].unique()
 
-plt.plot(dates, cases)
+    # Define things that are not countries and remove them from the countries list
+    notCountries = ['Africa', 'Asia', 'Central African Republic', 'Europe', 'European Union', 'High income', 'International', 'Low income', 'Lower middle income', 'North America', 'Oceania', 'South Africa', 'South America', 'Upper middle income', 'World']
+    clist = list(clist)
+    for i in notCountries:
+        clist.remove(i)
+    return clist
+
+df = loadData()
+clist = getCountriesList(df)
+
+typeList = {'New cases':'new_cases', 'New cases smoothed (7-day rolling average)':'new_cases_smoothed', 'Cummulative cases': 'total_cases', 'New deaths':'new_deaths', 'New deaths smoothed (7-day rolling average)':'new_deaths_smoothed', 'Cummulative deaths': 'total_deaths'}
+
+# Create the streamlit sidebar
+country = st.sidebar.selectbox("Select a country:",clist)
+dataType = st.sidebar.selectbox("Select the type of data you want to see:",typeList.keys())
+dataTypeSelection = typeList[dataType]
+
+normalized = st.sidebar.checkbox('Normalized data (per million habitants of the country)')
+if normalized:
+    dataType += " per million habitants of the country"
+    dataTypeSelection += "_per_million"
+# The header of the figure
+st.header("COVID-19 cases per country")
+
+cases = df[df['location'] == country][dataTypeSelection]
+dates = list(df[df['location'] == country]['date'])
+
+# Create a figure
+fig, ax = plt.subplots()
+# plot data
+ax.plot( dates, cases)
+ax.tick_params(axis='x',rotation=90)
+ax.set_xlabel("Date")
+ax.set_ylabel(dataType)
+st.write(country)
+
+
+# Plots the chart
+st.plotly_chart(fig)
