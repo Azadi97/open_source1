64f1dbbe866522c284bbca6f7ba1ae30c8b39aef
Azadi97 <gworld97@gmail.com>
2022-04-16 13:30:14 +0200

    Update cov_dashboard1.py

--- cov_dashboard1.py
+++ cov_dashboard1.py
@@ -32,7 +32,7 @@
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
